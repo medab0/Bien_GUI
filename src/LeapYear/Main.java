fb13008250acbd327795822712b6c8d6a37c0a2e
LAB12_USER ON WS28 <L12Y16W28@citulab.net>
2023-11-16 16:24:58 +0800

    push

--- src/LeapYear/Main.java
+++ src/LeapYear/Main.java
@@ -1,4 +1,51 @@
 package LeapYear;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main extends JFrame {
+    private JTextField tfYear;
+    private JLabel lInputYear;
+    private JButton btnCheckYear;
+    private JPanel jpanel;
+
+    public Main() {
+
+
+        btnCheckYear.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                    int year = Integer.parseInt(tfYear.getText());
+
+                    boolean isLeapYear = false;
+
+                    if (year % 4 == 0) {
+                        if (year % 100 == 0) {
+                            isLeapYear = (year % 400 == 0);
+                        } else {
+                            isLeapYear = true;
+                        }
+                    }
+
+                    if (isLeapYear) {
+                        JOptionPane.showMessageDialog(Main.this, "Leap Year");
+                    } else {
+                        JOptionPane.showMessageDialog(Main.this, "Not a Leap Year");
+                    }
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Main app = new Main();
+            app.setContentPane(app.jpanel);
+            app.setSize(300, 150);
+            app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            app.setVisible(true);
+            app.setTitle("Leap Year Checker");
+        });
+    }
 }
